cc4a522d59a6345247d3e2e231c0b6d48e82474e
Amil Masic <amil.masic@gmail.com>
2018-08-30 20:19:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,7 +31,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == true || board[index] == " "
+  if position_taken?(board, index) == true
       false
     else
       true
