aab366dbbf4d530cf821e8ba18d80e0a8ee89840
Amil Masic <amil.masic@gmail.com>
2018-08-30 20:50:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,18 +64,18 @@ def valid_move?(board, index)
   end
 end
 
-# def turn(board)
-#   puts "Please enter 1-9:"
-#   input = gets.strip
-#   index = input_to_index(input)
-#   if valid_move?(board, index)
-#     move(board, index)
-#     display_board(board)
-#   else
-#     puts "oops! that's not a valid move."
-#     turn(board)
-#   end
-# end
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    puts "oops! that's not a valid move."
+    turn(board)
+  end
+end
 
 # def turn_count(board)
 #   tokens = ["X", "O"]
