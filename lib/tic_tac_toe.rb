918ef61a2f004f2ebeba7a3f5580f27fe3d39c97
Amil Masic <amil.masic@gmail.com>
2018-08-30 21:53:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -118,11 +118,11 @@ end
 
 def play(board)
   until over?(board) == true
-    # input = turn(board)
-    # if won?(board) == true
-    #   print "Congratulations player #{board[token]}"
-    # elsif draw?(board) == true
-    #   print "Cat's Game!"
-    # end
+    input = turn(board)
+    if won?(board) == true
+      print "Congratulations player #{board[token]}"
+    elsif draw?(board) == true
+      print "Cat's Game!"
+    end
   end
 end
