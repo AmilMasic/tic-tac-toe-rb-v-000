7d3a42838b61ddbef33de92ba48fd50f66177181
Amil Masic <amil.masic@gmail.com>
2018-08-30 20:15:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,7 +31,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index) || board[index] == nil
+  if position_taken?(board, index) == false || board[index] == nil
       true
     else board[index] == index.between?(0,8)
       false
